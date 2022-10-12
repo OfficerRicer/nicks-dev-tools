105f6261ed6f38f4de5a874f2fec84b877c49111
OfficerRicer <52828546+OfficerRicer@users.noreply.github.com>
2022-10-12 12:50:18 -0400

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -2,18 +2,18 @@ fx_version 'cerulean'
 
 game { 'gta5' }
 
-description 'Globbing Template made by PLOK'
+description 'fxmanifest.lua made by NickR#8733'
 
 files {
-    'data/**/*.meta'
+    '*.meta'
 }
  
-data_file 'HANDLING_FILE' 'data/**/handling.meta'
-data_file 'VEHICLE_LAYOUTS_FILE' 'data/**/vehiclelayouts.meta'
-data_file 'VEHICLE_METADATA_FILE' 'data/**/vehicles.meta'
-data_file 'CARCOLS_FILE' 'data/**/carcols.meta'
-data_file 'VEHICLE_VARIATION_FILE' 'data/**/carvariations.meta'
+data_file 'HANDLING_FILE' 'handling.meta'
+data_file 'VEHICLE_LAYOUTS_FILE' 'vehiclelayouts.meta'
+data_file 'VEHICLE_METADATA_FILE' 'vehicles.meta'
+data_file 'CARCOLS_FILE' 'carcols.meta'
+data_file 'VEHICLE_VARIATION_FILE' 'carvariations.meta'
 -- data_file 'DLCTEXT_FILE' 'data/**/dlctext.meta'
 -- data_file 'CARCONTENTUNLOCKS_FILE' 'data/**/carcontentunlocks.meta'
 
-client_script 'vehicle_names.lua'
+--client_script 'vehicle_names.lua'
